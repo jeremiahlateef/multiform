e407dc18c692fb8019e50969a488472ee2520ea4
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-03-27 06:53:11 +0100

    components for the four section

---
+++ src/components/Step1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Step1 = () => {
+  return <h2>Test</h2>;
+};
+
+export default Step1;
---
+++ src/components/Step2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Step2 = () => {
+  return <h2 className="text-3xl">Test2</h2>;
+};
+
+export default Step2;
