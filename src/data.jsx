3f51fb23bf40ca6aa17fa021ac89b9bcbf0ad854
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-03-27 06:55:03 +0100

    chore

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,7 @@
-import Step1 from "./Step1";
-import Step2 from "./Step2";
-import Step3 from "./Step3";
-import Step4 from "./Step4";
+import Step1 from "./components/Step1";
+import Step2 from "./components/Step2";
+import Step3 from "./components/Step3";
+import Step4 from "./components/Step4";
 const formsPage = [
   {
     id: 1,
