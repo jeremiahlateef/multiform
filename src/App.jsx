e1c64c07a7dbafb6fec53688561ed418da81f21f
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-03-27 06:54:26 +0100

    renamed the path of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import data from "./data";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const [pages, setPages] = useState(data);
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline font-ubuntu">Hello world!</h1>
+      {pages.map((pager) => {
+        const { id, page } = pager;
+        return <div key={id}>{page()}</div>;
+      })}
     </div>
   );
 }
