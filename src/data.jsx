144acd3d6ae217e938debec999cdbfeb062c5833
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-03-28 19:40:39 +0100

    added more data for the footer buttons

--- src/data.jsx
+++ src/data.jsx
@@ -6,18 +6,38 @@ const formsPage = [
   {
     id: 1,
     page: () => <Step1 />,
+    buttons: 1,
+    step: "step 1",
+    info: "your info",
+    footerOne: "",
+    footerTwo: "next step",
   },
   {
     id: 2,
     page: () => <Step2 />,
+    buttons: 2,
+    step: "step 2",
+    info: "select plan",
+    footerOne: "go back",
+    footerTwo: "next step",
   },
   {
     id: 3,
     page: () => <Step3 />,
+    buttons: 3,
+    step: "step 3",
+    info: "add-ons",
+    footerOne: "go back",
+    footerTwo: "next step",
   },
   {
     id: 4,
     page: () => <Step4 />,
+    buttons: 4,
+    step: "step 4",
+    info: "summary",
+    footerOne: "go back",
+    footerTwo: "confirm",
   },
 ];
 export default formsPage;
