53e5307f2109d71c72c1f647b0595cf5d106fd2a
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-04-02 22:48:40 +0100

    chore: added notes to the page

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Pages = ({ pager, value }) => {
-  // This selects the pages individually from the data.
+  // The pager selects the pages individually from the data[value].
   const { page } = pager[value];
   return (
     <div className="bg-white shadow-lg w-4/5 mx-auto rounded-md top-24 sm:top-28   absolute left-1/2 transform -translate-x-1/2 z-10 md:static md:translate-x-0 md:translate-y-0  md:shadow-none md:w-full md:px-0 px-4 py-6">
