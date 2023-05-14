bca22a5d2c96972551e6ce0a476d46d5087e4b1a
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-05-14 19:00:40 +0100

    extracted the value from Step1 useContext

--- src/components/Step4.jsx
+++ src/components/Step4.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./Header";
+import { Step1Context } from "./Step1";
 
 const Step4 = () => {
+  const value = useContext(Step1Context);
+  console.log(value);
   return (
     <>
       <div className="">
         <Header number={3} />
+        <div className="p-4">
+          {/* <p>{userInput?.name}</p> */}
+          <p>Hello</p>
+        </div>
       </div>
     </>
   );
