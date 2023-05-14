844e3944de33ef031a9791c8bbb1071151d5e6ee
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-05-14 19:09:17 +0100

    added the handleSubmit function

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Pages from "./Pages";
 import { useState } from "react";
+import { useCallback } from "react";
 
 const Footer = ({ setFormValue, footBtn, value, setValue }) => {
   // use an array to display just one item from the data
+  // const { handleSubmit, setUserInput } = useStep1Context;
   const { footerOne, footerTwo } = footBtn[value];
 
   const handleBtnPrev = () => {
     setValue(value >= 0 ? value - 1 : value);
   };
   const handleBtnNext = () => {
-    setValue(value <= 2 ? value + 1 : value);
-    setFormValue();
+    if (setValue(value === 0)) {
+      const moveFow = () => {
+        // value + 1;
+        // handleSubmit();
+      };
+      return moveFow();
+    } else {
+      return setValue(value <= 2 ? value + 1 : value);
+    }
+    // setFormValue();
   };
   return (
     <footer className="bg-white absolute md:relative md:bottom-auto bottom-0 h-20 w-full md:mx-auto right-0 md:w-full">
