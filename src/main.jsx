316d6f79b4fd3d8847d9cfefe47d417e5c4e4d10
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-05-17 00:11:44 +0100

    wrapped the App with the context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ContextProvider } from "./ContextApp";
+import Step1 from "./components/Step1";
+import Step4 from "./components/Step4";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+      <Step1 />
+      <Step4 />
+    </ContextProvider>
   </React.StrictMode>
 );
